df46918a6f17bdde54ec8e8b47af3f07d2a55b96
mbichoh <nattymbichoh@gmail.com>
2020-03-06 08:49:26 +0300

    serving static files and tmpl go templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,10 @@ func main() {
 	mux.HandleFunc("/snippet/", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	//handling static files
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Server starting on port http://localhost:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
